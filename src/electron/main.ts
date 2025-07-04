e9223f0ae3fb9eecc3ad3595ac3ff6e80d966a39
Iyanuoluwa Taiwo <iyanuoluwa938@gmail.com>
2025-07-05 00:16:28 +0100

    electron set app set up

---
+++ src/electron/main.ts
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+
+  mainWindow.webContents.openDevTools();
+});
