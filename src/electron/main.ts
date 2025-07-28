67b77aba295d761bf4e4deb1e556fbb263a82a51
Iyanuoluwa Taiwo <iyanuoluwa938@gmail.com>
2025-07-28 05:37:09 +0100

    minor production changes

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,9 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, Menu } from "electron";
 import { initialize, enable } from "@electron/remote/main/index.js";
 import { waitForServer } from "./utils/server.js";
 import { isDevMode } from "./utils/config.js";
 import path from "path";
-import { fileURLToPath } from "url";
+// import { fileURLToPath } from "url";
 
 // Initialize IPC handlers
 import "./ipc-handlers/directory.js";
@@ -11,24 +11,31 @@ import "./ipc-handlers/scan.js";
 import "./ipc-handlers/quarantine.js";
 import "./ipc-handlers/vulnerabilities.js";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 async function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 600, // Set minimum width
+    minHeight: 400, // Set minimum height
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
+
+  if (process.platform === "darwin") {
+    Menu.setApplicationMenu(null);
+  }
   enable(win.webContents);
 
   if (isDevMode()) {
     await waitForServer("http://localhost:5123");
     win.loadURL("http://localhost:5123");
   } else {
-    win.loadFile(path.join(__dirname, "../../dist-react/index.html"));
+    const indexPath = path.join(app.getAppPath(), "dist-react", "index.html");
+    win.loadFile(indexPath);
   }
 
   return win;
